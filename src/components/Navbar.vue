54c192489205aeee12c393c7aeb50a2c6fee0cfc
Bruno Dias <brunodg.dev@gmail.com>
2024-03-13 21:15:18 -0300

    [fix] new deploy and added some changes to dark mode

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,25 +10,25 @@ const title = ref('ShiandeGames');
 </script>
 
 <template>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav class="bg-white border-gray-200 dark:bg-indigo-950 dark:border-gray-700">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
             <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <span
                     class="self-center text-3xl font-sega text-indigo-800 whitespace-nowrap dark:text-white">{{title}}</span>
             </a>
             <div class="max-w-screen-xl">
                 <a href="#"
-                    class="text-gray-800 border-b-2 border-transparent hover:text dark:hover:text-gray-200 hover:border-indigo-800 mx-1.5 sm:mx-6">Home</a>
+                    class="text-gray-800 dark:text-gray-100 border-b-2 border-transparent hover:text dark:hover:text-gray-200 hover:border-indigo-700 mx-1.5 sm:mx-6">Home</a>
 
                 <a href="#"
-                    class="text-gray-800 border-b-2 border-transparent hover:text dark:hover:text-gray-200 hover:border-indigo-800 mx-1.5 sm:mx-6"
+                    class="text-gray-800 dark:text-gray-100 border-b-2 border-transparent hover:text dark:hover:text-gray-200 hover:border-indigo-700 mx-1.5 sm:mx-6"
                     @click="showConsoleMenu">Consoles</a>
                 <div v-if="active">
                     Menu
                 </div>
 
                 <a href="#"
-                    class="text-gray-800 border-b-2 border-transparent hover:text dark:hover:text-gray-200 hover:border-indigo-800 mx-1.5 sm:mx-6">Contato</a>
+                    class="text-gray-800 dark:text-gray-100 border-b-2 border-transparent hover:text dark:hover:text-gray-200 hover:border-indigo-700 mx-1.5 sm:mx-6">Contato</a>
             </div>
         </div>
     </nav>
